9ab8403bd68f6feff30f8fe757f959fdda9a14d0
Akash <akhvnd@gmail.com>
2022-08-01 14:01:40 +0530

    modified:Inserting Elements

--- LinkList.java
+++ LinkList.java
@@ -1,10 +1,15 @@
 package com.javashowcase;
 
+import java.util.Scanner;
+
 public class LinkList<T> {
 	
+	
+	Scanner sc = new Scanner(System.in);
 	MyNode head = null;
 	private MyNode tail = null;
 	private MyNode temp = null;
+	private MyNode pointer = null;
 	
 	public void append(MyNode<T> newnode) {
 		if(head == null && tail == null) {
@@ -28,6 +33,23 @@ public class LinkList<T> {
 			temp = newnode;
 			temp.setNext(head);
 			head = newnode;
+		}
+	}
+	
+	public void insert(MyNode<T> newnode ,MyNode<T>  oldnode) {
+		pointer = head;
+		int flag = 0;
+		while(pointer != null) {
+			if(pointer.getKey() == oldnode.getKey()) {
+				newnode.setNext(pointer.getNext());
+				pointer.setNext(newnode);
+				flag++;
+			}
+			pointer = pointer.getNext();
+			//return head;
+		}
+		if(flag == 0) {
+			System.out.println("Node does not exist");
 		}
 	}
 	
--- MyNodeTest.java
+++ MyNodeTest.java
@@ -1,26 +1,68 @@
 package com.javashowcase;
 
-public class MyNodeTest {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-    	MyNode<Integer> myFirstNode = new MyNode<>(56);
-    	MyNode<Integer> mySecondNode = new MyNode<>(30);
-    	MyNode<Integer> myThirdNode = new MyNode<>(70);
-    	
+public class MyNodeTest {
+ 
+   static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {  	
     	LinkList<Integer> list = new LinkList<>();
-    	/*list.add(myFirstNode);
-    	list.add(mySecondNode);
-    	list.add(myThirdNode);
+        while(true) {
+        	System.out.println();
+        	System.out.println("Enter 0. To add\nEnter 1. To Display\nEnter 2. To Insert\nEnter 3. To Exist");
+        	System.out.println("Enter your choice");
+            int choice = sc.nextInt();
+        switch(choice) {
+        
+            case 0 :System.out.println("Enter the value you want to add");
+                    Integer number = sc.nextInt();
+                    MyNode<Integer> newnode = new MyNode<>(number);
+                    list.append(newnode);
+                    break;
+                    
+            case 1 :LinkListIterator<Integer> ti = new LinkListIterator<>();
+                    ti.iterator(list);
+                    break;
+                     
+            case 2 :System.out.println();
+                    System.out.println("Enter the value you want to add");
+                    Integer addnumber = sc.nextInt();
+                    MyNode<Integer> node = new MyNode<>(addnumber);
+                    System.out.println("Enter the number after which new number to be inserted");
+                    Integer previousnumber = sc.nextInt();
+                    MyNode<Integer> oldnode = new MyNode<>(previousnumber);
+                    list.insert(node , oldnode);
+        
+        }
+         if(choice == 3) {break;}
+        }
     	
-    	LinkListIterator<Integer> it = new LinkListIterator<>();
-        it.iterator(list);*/
+    	/*System.out.println("Enter the value you want to add");
+        Integer number = sc.nextInt();
+        MyNode<Integer> newnode = new MyNode<>(number);
+        list.append(newnode);
     	
-        list.append(myFirstNode);
-        list.append(mySecondNode);
-        list.append(myThirdNode);
+        //LinkList<Integer> list = new LinkList<>();
+    	//list.append(myFirstNode);
+        //list.append(myThirdNode);
+        
         
         LinkListIterator<Integer> ti = new LinkListIterator<>();
         ti.iterator(list);
+        
+        System.out.println();
+        System.out.println("Enter the value you want to add");
+        Integer addnumber = sc.nextInt();
+        MyNode<Integer> node = new MyNode<>(addnumber);
+        System.out.println("Enter the number after which new number to be inserted");
+        Integer previousnumber = sc.nextInt();
+        MyNode<Integer> oldnode = new MyNode<>(previousnumber);
+        list.insert(node , oldnode);
+        
+        LinkListIterator<Integer> ti1 = new LinkListIterator<>();
+        ti1.iterator(list);*/
+        
 	}
 
 }
